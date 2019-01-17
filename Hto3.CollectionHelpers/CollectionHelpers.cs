ee4901d51de0bbb21ab026b53ce37ac60bf63fd9
Hélio Tibagí <helio_net@hotmail.com>
2019-01-17 21:04:15 -0200

    Translating the docs

--- Hto3.CollectionHelpers/CollectionHelpers.cs
+++ Hto3.CollectionHelpers/CollectionHelpers.cs
@@ -14,10 +14,10 @@ namespace Hto3.CollectionHelpers
     public static class CollectionHelpers
     {
         /// <summary>
-        /// If the collection is null, then it returns an empty collection
+        /// If the collection is null then returns an empty collection
         /// </summary>
-        /// <typeparam name="T">Tipo da coleção</typeparam>
-        /// <param name="source">Coleção a testar se é nulo</param>
+        /// <typeparam name="T">Collection item type</typeparam>
+        /// <param name="source">Collection to test if is null</param>
         /// <returns></returns>
         public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> source)
         {
@@ -25,24 +25,23 @@ namespace Hto3.CollectionHelpers
         }
 
         /// <summary>
-        /// If the collection is null, then it returns an empty collection
+        /// If the collection is null then returns an empty collection
         /// </summary>
-        /// <param name="source">Coleção a testar se é nulo</param>
+        /// <param name="source">Collection to test if is null</param>
         /// <returns></returns>
         public static IEnumerable EmptyIfNull(this IEnumerable source)
         {
             return source ?? Enumerable.Empty<Object>();
         }
         /// <summary>
-        /// Describes a list in a user-friendly String. <example>Lista de Int32: 1, 2, 3, 4 ou 1, 2, 3 e 4</example>
+        /// Describes a list in a user-friendly String allowing you to define the format of the item (like String.Format), and the string to use to separate the items. Similar experience like String.Join <example>List of Int32: 1, 2, 3, 4</example>
         /// </summary>
-        /// <typeparam name="T">Tipo da coleção</typeparam>
-        /// <param name="collection">Lista do que implemente a Interface IList</param>
-        /// <param name="comLetraE">Se verdadeiro, coloca a letra "e" entre o penúltimo e o último elemento da lista</param>
-        /// <param name="format">String de Formato, a mesma empregada no <i>String.Format</i>. É necessário ter um, e somente um token {0}.</param>
-        /// <param name="separator">Texto que separa os itens para descreve-los</param>
-        /// <returns>String contendo a lista separada por virgulas</returns>
-        public static String DescreverColecao<T>(this IEnumerable<T> collection, String format = "{0}", String separator = ", ")
+        /// <typeparam name="T">Collection type</typeparam>
+        /// <param name="collection">The collection</param>
+        /// <param name="format">Format string, the same as used in <i>String.Format</i> method. It's necessary to use one, and only one {0} token.</param>
+        /// <param name="separator">String to separate the itens</param>
+        /// <returns></returns>
+        public static String Describe<T>(this IEnumerable<T> collection, String format = "{0}", String separator = ", ")
         {
             if (!format.Contains("{0") || !format.Contains("}"))
                 throw new FormatException("O formato deve ter um e somente um token {0}!");
@@ -63,8 +62,7 @@ namespace Hto3.CollectionHelpers
         /// <summary>
         /// Check if the execution stack is within a CollectionChanged call of an ObservableCollection
         /// </summary>
-        /// <typeparam name="T">Tipo do item da coleção</typeparam>
-        /// <param name="collection">A instância da ObservableCollection</param>
+        /// <param name="collection">The notificable collection</param>
         /// <returns></returns>
         public static Boolean IsReentrancy(this INotifyCollectionChanged collection)
         {
@@ -82,13 +80,13 @@ namespace Hto3.CollectionHelpers
             return monitorIsBusy;
         }
         /// <summary>
-        /// I build a list from a string separated by a separator character.
+        /// I build a list from a delimited string.
         /// </summary>
-        /// <typeparam name="TReturn">Tipo da coleção a ser construída</typeparam>
-        /// <param name="delimitedList">texto contendo uma lista de valores delimitados de uma maneira padrão</param>
-        /// <param name="separator">separador padrão do texto</param>
+        /// <typeparam name="TReturn">Type of the itens of the collection</typeparam>
+        /// <param name="delimitedList">String containing a list of delimited values</param>
+        /// <param name="separator">Separator that is used to delimit the itens</param>
         /// <returns></returns>
-        public static IEnumerable<TReturn> ConstruirColecao<TReturn>(this String delimitedList, String separator)
+        public static IEnumerable<TReturn> BuildCollectionFromString<TReturn>(this String delimitedList, String separator)
         {
             foreach (var item in delimitedList.Split(new String[] { separator }, StringSplitOptions.RemoveEmptyEntries))
                 yield return (TReturn)Convert.ChangeType(item, typeof(TReturn));
@@ -96,9 +94,9 @@ namespace Hto3.CollectionHelpers
         /// <summary>
         /// Removes all elements that satisfy the condition defined by the specified predicate
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="collection">A coleção a se trabalhar.</param>
-        /// <param name="match">Um delegate com o predicado que define as condições para escolher os elementos a remover.</param>
+        /// <typeparam name="T">Type of the collection item</typeparam>
+        /// <param name="collection">The collection</param>
+        /// <param name="match">A delegate with the predicate that defines the conditions for choosing which elements to remove.</param>
         public static void RemoveAll<T>(this ObservableCollection<T> collection, Func<T, Boolean> match)
         {
             for (var i = collection.Count - 1; i >= 0; i--)
@@ -110,11 +108,11 @@ namespace Hto3.CollectionHelpers
         /// <summary>
         /// Replaces an item in the collection
         /// </summary>
-        /// <typeparam name="T">Tipo da coleção genérica</typeparam>
-        /// <param name="collection">Coleção</param>
-        /// <param name="oldItem">Item velho (o item velho precisa existir)</param>
-        /// <param name="newItem">Item novo</param>
-        public static void SubstituirItem<T>(this ObservableCollection<T> collection, T oldItem, T newItem)
+        /// <typeparam name="T">Type of the collection item</typeparam>
+        /// <param name="collection">The collection</param>
+        /// <param name="oldItem">Old item (old item must exist)</param>
+        /// <param name="newItem">The new item</param>
+        public static void ReplaceItem<T>(this ObservableCollection<T> collection, T oldItem, T newItem)
         {
             var indexDoVelho = collection.IndexOf(oldItem);
             if (indexDoVelho == -1)
@@ -127,33 +125,33 @@ namespace Hto3.CollectionHelpers
         /// <summary>
         /// Replaces an item in the collection
         /// </summary>
-        /// <typeparam name="T">Tipo da coleção genérica</typeparam>
-        /// <param name="collection">Coleção</param>
-        /// <param name="index">Index do item a ser substituído</param>
-        /// <param name="newItem">Item novo</param>
-        public static void SubstituirItem<T>(this ObservableCollection<T> collection, Int32 index, T newItem)
+        /// <typeparam name="T">Type of the collection item</typeparam>
+        /// <param name="collection">The collection</param>
+        /// <param name="index">The index of the item to replace</param>
+        /// <param name="newItem">The new item</param>
+        public static void ReplaceItem<T>(this ObservableCollection<T> collection, Int32 index, T newItem)
         {
             typeof(ObservableCollection<T>)
                 .GetMethod("SetItem", BindingFlags.NonPublic | BindingFlags.Instance)
                 .Invoke(collection, new Object[] { index, newItem });
         }
         /// <summary>
-        /// Aplaina (flat) uma estrutura de árvore
+        /// Flatten a tree structure
         /// </summary>
-        /// <typeparam name="T">Tipo do item dos nós da árvore</typeparam>
-        /// <param name="tree">Árvore</param>
-        /// <param name="branchLocator">Expressão que informa a prorpiedade das folhas</param>
+        /// <typeparam name="T">Tree nodes item type</typeparam>
+        /// <param name="tree">The Tree</param>
+        /// <param name="branchLocator">Expression that indicate where is the property of the leaves</param>
         /// <returns></returns>
         public static IEnumerable<T> FlatTree<T>(this IEnumerable<T> tree, Func<T, IEnumerable<T>> branchLocator)
         {
             return tree.EmptyIfNull().SelectMany(c => branchLocator(c).FlatTree(branchLocator)).Concat(tree.EmptyIfNull());
         }
         /// <summary>
-        /// Replaces all items in an ObservableCollection with other items. It would be the same as calling the Clear() method and then adding the new items.
+        /// Replaces all items in an ObservableCollection with other items. It would be the same as calling the <i>Clear()</i> method and then adding the new items, but reducing the procedure to only one step to improve performance.
         /// </summary>
-        /// <typeparam name="T">Tipos dos itens da coleção</typeparam>
-        /// <param name="observableCollection">Coleção a se trabalhar</param>
-        /// <param name="toAdd">Itens a adicionar na coleção</param>
+        /// <typeparam name="T">Type of the collection item</typeparam>
+        /// <param name="observableCollection">The observable collection</param>
+        /// <param name="toAdd">Items to add</param>
         public static void ReplaceAllBy<T>(this ObservableCollection<T> observableCollection, IEnumerable<T> toAdd)
         {
             if (observableCollection == null || !toAdd.EmptyIfNull().Any())
@@ -174,11 +172,11 @@ namespace Hto3.CollectionHelpers
             type.InvokeMember("OnCollectionChanged", BindingFlags.Instance | BindingFlags.InvokeMethod | BindingFlags.NonPublic, null, observableCollection, new object[] { new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset) });
         }
         /// <summary>
-        /// Add multiple items to a collection
+        /// Add multiple items to a collection invoking the change event only one time, at the end.
         /// </summary>
-        /// <typeparam name="T">Tipo dos itens da coleção</typeparam>
-        /// <param name="observableCollection">Instância da ObservableCollection</param>
-        /// <param name="toAdd">Coleção de itens a adicionar</param>
+        /// <typeparam name="T">>Type of the collection item</typeparam>
+        /// <param name="observableCollection">The observable collection</param>
+        /// <param name="toAdd">Items to add</param>
         public static void AddRange<T>(this ObservableCollection<T> observableCollection, IEnumerable<T> toAdd)
         {
             if (observableCollection == null || !toAdd.EmptyIfNull().Any())
@@ -288,7 +286,7 @@ namespace Hto3.CollectionHelpers
         /// <summary>
         /// Adds an item only if it does not exist in the collection
         /// </summary>
-        /// <typeparam name="T">Tipo do item</typeparam>
+        /// <typeparam name="T">Type of item</typeparam>
         /// <param name="list">Coleção de dados</param>
         /// <param name="predicate">Pedicado que testa a existência do item</param>
         /// <param name="item">Item a adicionar</param>
@@ -303,8 +301,8 @@ namespace Hto3.CollectionHelpers
         /// <summary>
         /// Removes an item only if it exists in the collection
         /// </summary>
-        /// <param name="list">Coleção de dados</param>
-        /// <param name="item">Item a remover</param>
+        /// <param name="list">List</param>
+        /// <param name="item">Item to remove</param>
         public static void RemoveIfExists(this IList list, Object item)
         {
             if (list == null)
@@ -318,7 +316,7 @@ namespace Hto3.CollectionHelpers
         /// </summary>
         /// <param name="collection">Coleção de dados</param>
         /// <returns>O tipo dos itens</returns>
-        public static Type ObterTipoDoItemDaColecao(this ICollection collection)
+        public static Type GetItemType(this ICollection collection)
         {
             var enumerable_type = collection.GetType()
                 .GetInterfaces()
@@ -329,16 +327,42 @@ namespace Hto3.CollectionHelpers
                 return enumerable_type.GenericTypeArguments[0];
 
             if (collection.Count == 0)
-                throw new InvalidOperationException("Não é possível obter o tipo de uma coleção ICollection que não possui nenhum item.");
+                throw new InvalidOperationException("Cannot discover the collection item type in an empty ICollection.");
 
             return collection.First().GetType();
+        }
+        /// <summary>
+        /// Gets the symmetric difference of two sets using an equality comparer.
+        /// The symmetric difference is defined as the set of elements which are in one of the sets, but not in both.
+        /// </summary>
+        /// <remarks>
+        /// If one set has duplicate items when evaluated using the comparer, then the resulting symmetric difference will only
+        /// contain one copy of the the duplicate item and only if it doesn't appear in the other set.
+        /// </remarks>
+        /// <typeparam name="TSource">The type of elements in the collection.</typeparam>
+        /// <param name="value">The first enumerable.</param>
+        /// <param name="secondSet">The second enumerable to compare against the first.</param>
+        /// <param name="comparer">The comparer object to use to compare each item in the collection.  If null uses EqualityComparer(T).Default.</param>
+        /// <returns></returns>
+        public static IEnumerable<TSource> SymmetricDifference<TSource>(this IEnumerable<TSource> value, IEnumerable<TSource> secondSet, IEqualityComparer<TSource> comparer)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+            if (secondSet == null)
+                throw new ArgumentNullException(nameof(value));
+            
+            if (comparer == null)
+                comparer = EqualityComparer<TSource>.Default;
+
+            var result = value.Except(secondSet, comparer).Union(secondSet.Except(value, comparer), comparer);
+            return result;
         }
         /// <summary>
         /// Gets the type of items in a homogeneous collection
         /// </summary>
         /// <param name="collection">Coleção de dados</param>
         /// <returns>O tipo dos itens</returns>
-        public static Type ObterTipoDoItemDaColecao(this IEnumerable collection)
+        public static Type GetItemType(this IEnumerable collection)
         {
             var enumerable_type = collection.GetType()
                 .GetInterfaces()
@@ -477,8 +501,8 @@ namespace Hto3.CollectionHelpers
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="enumeration">Coleção de dados a ser trabalhada com janela</param>
-        /// <param name="windowSize">Quantidade de itens em uma janela</param>
-        /// <param name="action">Trabalho a ser realizado em cada janela</param>
+        /// <param name="windowSize">Amount of items in each window</param>
+        /// <param name="action">Work to do in each window</param>
         public static void Window<T>(this IEnumerable<T> enumeration, Int32 windowSize, Action<IEnumerable<T>> action)
         {
             var percorridos = 0;
